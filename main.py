b73b05e1fe46cd7cc864536f3d17bf097e395aac
Valerio Dias <valeriods@gmail.com>
2021-12-19 15:44:54 -0300

    Finalização da interface

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ window.title('pyClima')
 window.wm_iconbitmap('images/weather.ico')
 
 label = Label(window, text='40º', bg='#FCA311', font= 'impact 50 bold').place(x=90, y=5)
-label = Label(window, text='nublado', bg='#FCA311', font= 'impact 10 bold').place(x=10, y=40)
+label2 = Label(window, text='nublado', bg='#FCA311', font= 'impact 10 bold').place(x=10, y=40)
 
 
 
